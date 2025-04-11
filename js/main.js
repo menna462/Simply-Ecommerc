0c80db708a80f18fe5e37368fa87a92759c0da37
dalia4133 <dalianasserr615@gmail.com>
2025-04-11 23:14:06 +0200

    code new

--- js/main.js
+++ js/main.js
@@ -142,3 +142,6 @@ var swiper = new Swiper(".swiper", {
         pagination: { el: ".swiper-pagination", clickable: true },
         navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }
     });
+
+
+    
